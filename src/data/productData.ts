5f719dec7002bddd6104572b46534b20a586e657
DarrylAdigwu <115912161+DarrylAdigwu@users.noreply.github.com>
2026-07-21 18:25:07 -0700

    css changes

---
+++ src/data/productData.ts
@@ -0,0 +1,12 @@
+import davisClose from '~/public/images/davis-solo-close.jpeg';
+import shelClose from '~/public/images/shel-solo-close.jpg';
+
+export const productData = [
+  {
+    id: 1,
+    title: "Product Title",
+    price: "$99.99",
+    src: davisClose, 
+    alt: "Image of the product"
+  },
+];
